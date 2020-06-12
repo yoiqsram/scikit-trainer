39b43ee5869b9494aea79134096ae35b392587b9
yoiqsram <yoiqrambadian@gmail.com>
2020-06-13 01:43:15 +0700

    change config format

--- sktrainer/_train.py
+++ sktrainer/_train.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, List, Tuple, Union, Iterable
+from typing import Callable, Dict, List, Tuple, Generator, Union
 
 import numpy as np
 import pandas as pd
@@ -12,34 +12,68 @@ from sktrainer._estimator import classifier, regressor
 __all__ = ['ModelTrainer', 'train']
 
 
+_estimator = {'reg': regressor, 'classif': classifier}
+
+
 def _check_estimator(estimator, task):
     return isinstance(estimator, BaseEstimator) and \
            ((isinstance(estimator, RegressorMixin) and task == 'reg') or
             (isinstance(estimator, ClassifierMixin) and task == 'classif'))
 
 
-class ModelTrainer(BaseEstimator):
-    _estimator = {'reg': regressor, 'classif': classifier}
+def _get_estimator(estimator, task):
+    if estimator in _estimator[task]:
+        return _estimator[task][estimator]()
+    elif _check_estimator(estimator(), task):
+        return estimator()
+    elif _check_estimator(estimator, task):
+        return estimator
 
-    _scoring = {
+    raise ValueError
+
+
+def _transform_config(config: Union[str, dict, list, tuple, Generator, Callable], task):
+    if config == 'all':
+        estimators = _estimator[task].values()
+        return [{'name': estimator.__class__.__name__, 'estimator': estimator()} for estimator in estimators]
+
+    elif isinstance(config, dict):
+        config['estimator'] = _get_estimator(config['estimator'], task)
+        config['name'] = config.get('name', config['estimator'].__class__.__name__)
+        return [config]
+
+    elif any(isinstance(config, t) for t in (list, tuple, Generator)):
+        _config = []
+        for conf in config:
+            _config.extend(_transform_config(conf, task))
+        return _config
+
+    else:
+        estimator = _get_estimator(config, task)
+        return [{'name': estimator.__class__.__name__, 'estimator': estimator}]
+
+
+class ModelTrainer(BaseEstimator):
+    _default_scoring = {
         'classif': ['accuracy'],
         'reg': ['neg_root_mean_squared_error']
     }
 
     def __init__(self, task: str = 'reg', config: Union[str, List, Tuple, Dict] = 'all',
-                 scoring: str = None, cv: int = 5, n_jobs: int = -1,
-                 verbose: int = 1, random_state: int = None):
+                 scoring: str = None, cv: int = 5, n_jobs: int = -1, verbose: int = 1,
+                 random_state: int = None, ):
         assert task in {'reg', 'classif'}
         self.task = task
 
-        self.config = self._get_config(config)
+        self.config = _transform_config(config, self.task)
 
         if isinstance(scoring, str):
             scoring = [scoring]
         elif scoring is None:
-            scoring = self._scoring[self.task]
+            scoring = self._default_scoring[self.task]
 
         self.scoring = scoring
+
         self.cv = cv
         self.n_jobs = n_jobs
         self.verbose = verbose
@@ -47,51 +81,31 @@ class ModelTrainer(BaseEstimator):
 
         self.summary = None
 
-    def _get_config(self, config: Union[str, Dict, Callable]):
-        if isinstance(config, type):
-            config = config()
+    def _fit(self, X, y, conf):
+        conf = conf.copy()
+        estimator = conf.pop('estimator')
+        estimator_name = conf.pop('name')
+        search = conf.pop('search', 'grid')
+        n_iter = conf.pop('n_iter', 5)
 
-        if isinstance(config, str):
-            if config == 'all':
-                return {estimator(): dict() for estimator in self._estimator[self.task].values()}
-            elif config in self._estimator[self.task]:
-                return {self._estimator[self.task][config](): dict()}
+        if self.verbose:
+            print(estimator_name, '\n'.join(f'  {name}: {str(v)}' for name, v in conf.items()), sep='\n')
 
-        elif _check_estimator(config, self.task):
-            return {config: dict()}
-
-        elif isinstance(config, Dict):
-            return {list(self._get_config(key).keys())[0]: value for key, value in config.items()}
-
-        elif isinstance(config, Iterable):
-            _config = dict()
-            for conf in config:
-                _config.update(self._get_config(conf))
-            return _config
-
-        raise ValueError
-
-    def _fit(self, X, y, estimator, params):
         prefix = ''
         if self.task == 'classif':
             prefix = 'base_estimator__'
             estimator = CalibratedClassifierCV(base_estimator=estimator)
 
-        search = params.pop('search', 'grid')
+        params = {prefix + name: v for name, v in conf.items()}
+
         if search == 'grid':
-            estimator_ = GridSearchCV(estimator,
-                                      {prefix + key: value for key, value in params.items()},
+            estimator_ = GridSearchCV(estimator, params,
                                       scoring=self.scoring,
                                       refit=self.scoring[0],
                                       cv=self.cv,
                                       n_jobs=self.n_jobs).fit(X, y)
         else:
-            n_iter = params.get('n_iter', 5)
-            if 'n_iter' in params:
-                del params['n_iter']
-
-            estimator_ = RandomizedSearchCV(estimator,
-                                            {prefix + key: value for key, value in params.items()},
+            estimator_ = RandomizedSearchCV(estimator, params,
                                             scoring=self.scoring,
                                             refit=self.scoring[0],
                                             cv=self.cv,
@@ -99,26 +113,28 @@ class ModelTrainer(BaseEstimator):
                                             n_iter=n_iter,
                                             random_state=self.random_state).fit(X, y)
 
-        return estimator_
+        self.estimators_.append(estimator_)
 
-    def _record_results(self, estimator_search, estimator_name):
-        self.estimators_.append(estimator_search)
+        for name, v in estimator_.cv_results_.items():
+            v = v[estimator_.best_index_]
+            v = {key[len(prefix):]: value for key, value in v.items()} if name == 'params' else v
 
-        result_ = {
-            key: value[i]
-            for key, value in estimator_search.cv_results_.items()
-            for i in range(len(estimator_search.cv_results_['params']))
-            if i == estimator_search.best_index_
-        }
+            if name in self.results_:
+                self.results_[name] = np.append(self.results_[name], v)
 
-        for key, value in result_.items():
-            if key in self.results_:
-                self.results_[key] = np.append(self.results_.get(key), value)
-                if key[:4] == 'rank':
-                    score_rank = self.results_['mean' + key[4:]]
-                    self.results_[key] = len(score_rank) - np.argsort(score_rank)
+                if name[:4] == 'rank':
+                    score_rank = self.results_['mean' + name[4:]]
+                    self.results_[name] = len(score_rank) - np.argsort(score_rank)
             else:
-                self.results_[key] = np.array([value])
+                self.results_[name] = np.array([v])
+
+        summary_ = {'model': estimator_name,
+                    'params': '; '.join(f'{name}: {v}' for name, v in self.results_['params'][-1].items())}
+        for scoring in self.scoring:
+            summary_['mean ' + scoring] = self.results_['mean_test_' + scoring][-1]
+            summary_['std ' + scoring] = self.results_['std_test_' + scoring][-1]
+
+        self.summary = self.summary.append(pd.Series(summary_), ignore_index=True)
 
         self.best_index_ = np.argmin(self.results_['rank_test_' + self.scoring[0]])
         self.best_score_ = self.results_['rank_test_' + self.scoring[0]][self.best_index_]
@@ -126,19 +142,14 @@ class ModelTrainer(BaseEstimator):
 
         if self.verbose:
             print('\nBest score:')
-            for key, values in result_.items():
-                if key[:9] == 'mean_test':
-                    print('  {}: {:.4f}'.format(key[10:], values), end=' ')
-                elif key[:8] == 'std_test':
-                    print('+/- {:.4f}'.format(values))
+            for name, v in self.results_.items():
+                if name[:9] == 'mean_test':
+                    print('  {}: {:.4f}'.format(name[10:], v[-1]), end=' ')
+                elif name[:8] == 'std_test':
+                    print('+/- {:.4f}'.format(v[-1]))
             print('=' * 50, end='\n\n')
 
-        summary_ = {'model': estimator_name, 'params': str(result_['params'])}
-        for scoring in self.scoring:
-            summary_['mean ' + scoring] = result_['mean_test_' + scoring]
-            summary_['std ' + scoring] = result_['std_test_' + scoring]
-
-        self.summary = self.summary.append(pd.Series(summary_), ignore_index=True)
+        return estimator_
 
     def fit(self, X, y):
         self.estimators_ = []
@@ -148,18 +159,8 @@ class ModelTrainer(BaseEstimator):
                                             [f'{prefix} {scoring}' for scoring in self.scoring
                                              for prefix in ('mean', 'std')])
 
-        for estimator, params in self.config.items():
-            estimator_name = estimator.__class__.__name__
-
-            params['search'] = params.get('search', 'grid')
-
-            if self.verbose:
-                print(estimator_name,
-                      '\n'.join(f'  {key}: {str(values)}' for key, values in params.items()),
-                      sep='\n')
-
-            estimator_ = self._fit(X, y, estimator, params)
-            self._record_results(estimator_, estimator_name)
+        for conf in self.config:
+            self._fit(X, y, conf)
 
         if self.verbose:
             print('Best model:')
@@ -181,6 +182,8 @@ class ModelTrainer(BaseEstimator):
             return self.best_estimator_
         elif isinstance(key, int):
             return self.estimators_[key]
+
+        raise KeyError
 
     def predict(self, X, key: int or str = 'best'):
         return self.get_estimator(key).predict(X)
